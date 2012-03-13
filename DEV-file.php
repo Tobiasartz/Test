d18b8b982f0c56d355aecc767cc5f78817de3d2f
Tobias Artz <tobiasartz@gmail.com>
2012-03-13 09:46:15 -0700

    Another commit

--- DEV-file.php
+++ DEV-file.php
@@ -1,7 +1,4 @@
 <?php
 echo 'This file should only be in the dev branch';
-<<<<<<< HEAD
 echo 'CONFLICT!!!';
-=======
 echo 'And this is an edit in the DEV branch on a file that is also in MASTER';
->>>>>>> dev
